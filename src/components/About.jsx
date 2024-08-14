e9f7a58fae8bd017b452953818f8acf224ac311c
Fatiha26 <f6lamia@gmail.com>
2024-08-14 22:59:33 +0600

    about img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,16 @@
 import Nav from "./nav"
-
+import about from "../assets/image/about.jpg"
 const About = () => {
   return (
     <>
       <Nav />
       <section>
          <div>
+           <img src={about} alt="about img" />
             
          </div>
          <div>
-          
+
          </div>
       </section>
     </>
