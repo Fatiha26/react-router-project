c956d6ceb8ef9e7af08d8ac528aa293e1524be9e
Fatiha26 <f6lamia@gmail.com>
2024-08-14 23:19:36 +0600

    home text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,16 @@ const Home = () => {
     <>
         <Nav />
         <section>
-           
+        <div>
+           <h2 className="font-bold text-3xl">Welcome to Our World</h2>
+           <p className="text-md mt-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, 
+              exercitationem! Repellendus, at. Explicabo corrupti dolorem fugit culpa cumque.
+           </p>
+           <button className="border border-slate-600 px-5 py-2 mt-4 text-lg font-bold"
+                   onClick={()=>learn()}>
+                   Learn More
+          </button>
+         </div>
         </section>
     </>
   )
