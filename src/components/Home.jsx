d7883f8a1fcdfea0e85df6dfe757c319374e2651
Fatiha26 <f6lamia@gmail.com>
2024-08-14 23:25:12 +0600

    home done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,11 +12,11 @@ const Home = () => {
   return (
     <>
         <Nav />
-        <section>
+        <section className="flex items-center gap-20 px-8 py-12">
         <div>
            <h2 className="font-bold text-3xl">Welcome to Our World</h2>
            <p className="text-md mt-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, 
-              exercitationem! Repellendus, at. Explicabo corrupti dolorem fugit culpa cumque.
+              exercitationem! Repellendus, at.<br /> Explicabo corrupti dolorem fugit culpa cumque.
            </p>
            <button className="border border-slate-600 px-5 py-2 mt-4 text-lg font-bold"
                    onClick={()=>learn()}>
@@ -24,7 +24,7 @@ const Home = () => {
           </button>
          </div>
          <div>
-           <img src={home} alt="about img" className=" w-auto" />           
+           <img src={home} alt="about img" className=" w-full h-96" />           
          </div>
         </section>
     </>
