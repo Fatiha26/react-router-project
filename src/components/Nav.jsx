88dbe29b54f126a2b71fbf0b8ccb8fc367c2b702
Fatiha26 <f6lamia@gmail.com>
2024-08-14 13:11:10 +0600

    styling nav done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,62 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <>
-      <section className="flex justify-around items-center py-4 bg-slate-200">
-         <div>
-           <a href="#" className="text-2xl uppercase font-bold">Logo</a>
-         </div>
-         <nav>
-            <ul className="flex ">
-              <li><NavLink to="/" className="py-4 px-8">Home</NavLink></li>
-              <li><NavLink to="/about" className="py-4 px-8">About</NavLink></li>
-              <li><NavLink to="/service" className="py-4 px-8">Services</NavLink></li>
-              <li><NavLink to="/project" className="py-4 px-8">Project</NavLink></li>
-              <li><NavLink to="/contact" className="py-4 px-8">Contact</NavLink></li>
-            </ul>
-         </nav>
-      </section>
-    </>
-  )
-}
+    <section className="flex justify-around items-center py-4 bg-slate-200">
+      <div>
+        <Link to="/" className="text-2xl uppercase font-bold">Logo</Link>
+      </div>
+      <nav>
+        <ul className="flex">
+          <li>
+            <NavLink 
+              to="/" 
+              className="py-4 px-8 text-md hover:text-slate-500"
+              activeClassName="text-slate-700 font-bold"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to="/about" 
+              className="py-4 px-8 text-md hover:text-slate-500"
+              activeClassName="text-slate-700 font-bold"
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to="/service" 
+              className="py-4 px-8 text-md hover:text-slate-500"
+              activeClassName="text-slate-700 font-bold"
+            >
+              Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to="/project" 
+              className="py-4 px-8 text-md hover:text-slate-500"
+              activeClassName="text-slate-700 font-bold"
+            >
+              Project
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to="/contact" 
+              className="py-4 px-8 text-md hover:text-slate-500"
+              activeClassName="text-slate-700 font-bold"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
 
-export default Nav
+export default Nav;
