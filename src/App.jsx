5e0cafba034f6c076c5f5393fc5b41b7ff6f462c
Fatiha26 <f6lamia@gmail.com>
2024-08-14 11:04:12 +0600

    react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Nav from "./components/nav"
+import About from "./components/About"
+import Services from "./components/Services"
+import Project from "./components/Project"
+import Contact from "./components/Contact"
+import Error from "./components/error"
 
 const App = () => {
   return (
-    <div className="text-red-400">hello</div>
+    <>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Nav />} />
+            <Route path="/about" element={<About />}/>
+            <Route path="/service" element={<Services />}/>
+            <Route path="/project" element={<Project />}/>
+            <Route path="/contact" element={<Contact />}/>
+            <Route path="*" element={<Error />}/>
+         </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
