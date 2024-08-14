4bdf1256cdb74428d245b4da2a127e244bbd2c1d
Fatiha26 <f6lamia@gmail.com>
2024-08-14 11:25:20 +0600

    navlink setup

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,23 @@
+import { NavLink } from "react-router-dom"
 
 const Nav = () => {
   return (
-    <div>nav</div>
+    <>
+      <section>
+         <div>
+           <a href="#">Logo</a>
+         </div>
+         <nav>
+            <ul>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/service">Services</NavLink></li>
+              <li><NavLink to="/project">Project</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+         </nav>
+      </section>
+    </>
   )
 }
 
